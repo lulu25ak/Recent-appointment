2344011739a08b91e73af29363f4f8c911a779f6
lulu25ak <lulu.ak25@gmail.com>
2025-04-22 18:56:50 +0300

    Add files via upload

---
+++ Dashboard.js
@@ -0,0 +1,6 @@
+document.getElementById("pet-count").textContent = 28;
+document.getElementById("appointment-count").textContent = 16;
+document.getElementById("client-count").textContent = 1;
+document.getElementById("treatment-count").textContent = 3;
+
+
